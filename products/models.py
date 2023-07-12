3cf95ed7e08e0a341699138aa363bfbf13844a4f
LudovicLeGuen <>
2023-07-12 17:47:40 +0000

    Added products app and inserted brands data

---
+++ products/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Brand(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+    brand_image_url = models.URLField(max_length=1024, null=True, blank=True)
+    brand_image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    discount = models.FloatField(blank=True, default=0)
+    discounted_price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    @property
+    def discount_in_percentage(self):
+        return f"{self.discount} %"
+
+    @property
+    def discounted_price(self):
+        return (self.price-((self.price*self.discount)/100))
+
+    def __str__(self):
+        return self.name
+
