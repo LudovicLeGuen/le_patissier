40f2fb832a365aafc6d65e9b47071636d6a44d95
LudovicLeGuen <>
2023-07-18 19:43:27 +0000

    Added Sorting Javascript

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category, Brand
 
 
@@ -16,21 +18,18 @@ def all_products(request):
     direction = None
 
     if request.GET:
-
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
-            print(sort)
-
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-
             products = products.order_by(sortkey)
 
         if 'category' in request.GET:
@@ -52,9 +51,12 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
@@ -79,6 +81,8 @@ def all_brands(request):
     products = Product.objects.all()
     names = None
     brand = None
+    sort = None
+    direction = None
 
     if request.GET:
 
@@ -87,9 +91,12 @@ def all_brands(request):
             products = products.filter(brand__name__in=brand)
             brand = Brand.objects.filter(name__in=brand)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'brands': brands,
         'products': products,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/all_brands.html', context)
