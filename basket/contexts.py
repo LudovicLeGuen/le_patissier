65f951263fdb635b0e56cee7a7fa809832ff5f60
LudovicLeGuen <>
2023-07-20 19:01:38 +0000

    Added function to add products to basket

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
 
+
 def basket_contents(request):
 
     basket_items = []
@@ -13,9 +14,9 @@ def basket_contents(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
-    
+
     context = {
         'basket_items': basket_items,
         'total': total,
