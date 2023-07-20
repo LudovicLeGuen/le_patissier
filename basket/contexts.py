27f125ff2b13f2a982ca487aa39b6f0b9a9e81ce
LudovicLeGuen <>
2023-07-20 19:20:50 +0000

    Updated contexts with basket content

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
 
 
 def basket_contents(request):
@@ -7,6 +9,21 @@ def basket_contents(request):
     basket_items = []
     total = 0
     product_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        product = get_object_or_404(Product, pk=item_id)
+        if product.discount == 0:
+            total += quantity * product.price
+        else:
+            total += quantity * product.discounted_price
+
+        product_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
