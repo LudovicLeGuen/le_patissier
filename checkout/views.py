a0433ca4156285d1e80523245ddffdc36fbc2792
LudovicLeGuen <>
2023-07-23 18:22:55 +0000

    Added stripe payment field in checkout with stripe elements.js

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NO3ChF76CufBdvKkJTW1jz01g98EPHMQ0kAGAwc33wdUd00p6ENtleKvlJeMcAojKGLwew18TSnoFPt6czl6Ggn00T2kI76nF',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
