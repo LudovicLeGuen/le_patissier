8726133152f992837d5bc7f2f8087289bbe6ba51
LudovicLeGuen <>
2023-07-27 19:46:38 +0000

    added order confirmation email

--- le_patissier/settings.py
+++ le_patissier/settings.py
@@ -176,6 +176,7 @@ STRIPE_CURRENCY = 'EUR'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.environ.get('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'lepatissier@example.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
