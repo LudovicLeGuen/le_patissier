4b72071f6c9f20a3c85ee8778cac0328a2caf87e
LudovicLeGuen <>
2023-08-10 19:33:58 +0000

    Added footer legal content pages, views and urls

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path("legal", views.legal, name="legal"),
+    path("privacy_policy", views.privacy_policy, name="privacy_policy"),
+    path("guarantee", views.guarantee, name="guarantee"),
+    path("terms_conditions", views.terms_conditions, name="terms_conditions"),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,3 +7,31 @@ def index(request):
     """ This view returns the index page """
 
     return render(request, 'home/index.html')
+
+
+def legal(request):
+    """
+    Legal Statement Page
+    """
+    return render(request, 'home/legal/legal.html')
+
+
+def terms_conditions(request):
+    """
+    Terms and conditions Page
+    """
+    return render(request, 'home/legal/terms_conditions.html')
+
+
+def privacy_policy(request):
+    """
+    Privacy policy Page
+    """
+    return render(request, 'home/legal/privacy_policy.html')
+
+
+def guarantee(request):
+    """
+    Guaranty Statement Page
+    """
+    return render(request, 'home/legal/guarantee.html')
